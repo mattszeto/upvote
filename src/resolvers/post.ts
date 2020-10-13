126d927e130466f501f08ee07077a154be5913e7
mattszeto <ms8288@gmail.com>
2020-10-12 22:27:39 -0700

    added deletepost to post resolver

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,4 +1,4 @@
-import { Resolver, Query, Ctx, Arg, Int, Mutation } from "type-graphql";
+import { Resolver, Query, Ctx, Arg, Mutation } from "type-graphql";
 import { Post } from "../entities/Post";
 import { MyContext } from "src/types";
 
@@ -15,7 +15,7 @@ export class PostResolver {
     // find post using id as parameter
     @Query(() => Post, { nullable: true })
     post(
-        @Arg("id", () => Int)  id: number,
+        @Arg("id")  id: number,
         @Ctx() {em}: MyContext
         ): Promise<Post | null> {
         return em.findOne(Post, { id });
@@ -50,5 +50,19 @@ export class PostResolver {
             await em.persistAndFlush(post);
         }
         return post;
+    }
+
+    // delete a post with post id
+    @Mutation(() => Boolean)
+    async deletePost(
+        @Arg("id") id: number,
+        @Ctx() {em}: MyContext 
+    ): Promise<boolean> {
+        try {
+            await em.nativeDelete(Post, {id})
+        } catch {
+            return false;
+        }
+        return true;
     }
 }
