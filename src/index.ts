cef8fb949c8db115713ca6ab5f3453692733e749
mattszeto <ms8288@gmail.com>
2020-10-14 17:41:36 -0700

    login and register using express-sessions

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,21 @@ import { MikroORM } from "@mikro-orm/core"
 import { __prod__ } from "./constants";
 import microConfig from "./mikro-orm.config";
 import express from 'express';
+import redis from 'redis';
+import session from 'express-session';
+import connectRedis from 'connect-redis';
+
 import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
 import { HelloResolver } from "./resolvers/hello";
 import { PostResolver } from "./resolvers/post";
 import { UserResolver } from "./resolvers/user";
+import { MyContext } from "./types";
 
 
 // main function for adding MikroORM to connect to postgreSQL (can see sql under the hood)
 const main = async () => {
-    
+
     const orm = await MikroORM.init(microConfig); // connect to database
     await orm.getMigrator().up(); //run migrations
     // run SQL
@@ -23,12 +28,34 @@ const main = async () => {
 
     const app = express();
 
-    const apolloServer = new ApolloServer ({
+    const RedisStore = connectRedis(session)
+    const redisClient = redis.createClient()
+
+    app.use(
+        session({
+            name: 'qid',
+            store: new RedisStore({
+                client: redisClient,
+                disableTouch: true,
+            }),
+            cookie: {
+                maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
+                httpOnly: true,
+                sameSite: 'lax', // protecting csrf
+                secure: __prod__ // cookie only works in https
+            },
+            saveUninitialized: false,
+            secret: "serhew4t3w4dfgssdfha",
+            resave: false,
+        })
+    );
+
+    const apolloServer = new ApolloServer({
         schema: await buildSchema({
             resolvers: [HelloResolver, PostResolver, UserResolver],
             validate: false
         }),
-        context: () => ({ em: orm.em })
+        context: ({ req, res }): MyContext => ({ em: orm.em, req, res })
     });
 
     // create graphql endpoint in express
--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,8 +1,9 @@
 import { User } from "../entities/User";
 import { MyContext } from "src/types";
-import { Resolver, Mutation, InputType, Field, Arg, Ctx } from "type-graphql";
+import { Resolver, Mutation, InputType, Field, Arg, Ctx, ObjectType, Query } from "type-graphql";
 
 import argon2 from "argon2";
+import e from "express";
 
 @InputType()
 class UsernamePasswordInput {
@@ -12,22 +13,126 @@ class UsernamePasswordInput {
     password: string;
 }
 
+@ObjectType()
+class FieldError {
+    @Field()
+    field: string;
 
-@Resolver()
-export class UserResolver {
-    //graphql query
-    @Mutation(() => User)
-    async register(
-        @Arg('options') options: UsernamePasswordInput,
-        @Ctx() {em}: MyContext
-    ) {
-        const hashedPass = await argon2.hash(options.password)
-        const user = em.create(User, {username: options.username, password: options.password})
-        await em.persistAndFlush(user);
-        return user;
-    }
+    @Field()
+    message: string;
 
 }
 
+@ObjectType()
+class UserResponse {
+    @Field(() => [FieldError], { nullable: true })
+    errors?: FieldError[];
+
+    @Field(() => User, { nullable: true })
+    user?: User;
+}
+
+@Resolver()
+export class UserResolver {
+
+    @Query(() => User, {nullable: true})
+    async me(
+        @Ctx() { req, em }: MyContext
+    ) {
+        // you are not logged in
+        if (!req.session!.userId) {
+            return null
+        }
+
+        const user = await em.findOne(User, { id: req.session!.userId})
+        return user;
+    }
 
 
+    @Mutation(() => UserResponse)
+    async register(
+        @Arg('options') options: UsernamePasswordInput,
+        @Ctx() { em, req }: MyContext
+    ): Promise<UserResponse> {
+        if (options.username.length <= 2) {
+            return {
+                errors: [
+                    {
+                        field: "username",
+                        message: "Length must be greater than 2"
+                    },
+                ],
+            };
+        }
+        if (options.password.length <= 3) {
+            return {
+                errors: [
+                    {
+                        field: "password",
+                        message: "Length must be greater than 3"
+                    },
+                ],
+            };
+        }
+        const hashedPass = await argon2.hash(options.password)
+        const user = em.create(User, { username: options.username, password: hashedPass })
+        try {
+            await em.persistAndFlush(user);
+        } catch (err) {
+            //duplicate username error
+            if (err.code == '23505') {//|| err.detail.includes("already exists")) {
+                return {
+                    errors: [
+                        {
+                            field: "username",
+                            message: "username already taken",
+                        },
+                    ],
+                };
+            }
+            console.log("message: ", err.message);
+        }
+        // store user id session, sets cookie on the user, keeps them logged in
+        // this will login user after they register
+        req.session!.userId = user.id;
+        return {
+            user,
+        };
+    }
+
+    @Mutation(() => UserResponse)
+    async login(
+        @Arg('options') options: UsernamePasswordInput,
+        @Ctx() { em, req }: MyContext
+    ): Promise<UserResponse> {
+        const user = await em.findOne(User, { username: options.username })
+        if (!user) {
+            return {
+                errors: [
+                    {
+                        field: "username",
+                        message: "That username doesn't exist"
+                    },
+                ],
+            };
+        }
+        const valid = await argon2.verify(user.password, options.password);
+        if (!valid) {
+            return {
+                errors: [
+                    {
+                        field: "password",
+                        message: "Incorrect password"
+                    },
+                ],
+            };
+        }
+
+        req.session!.userId = user.id;
+
+        return {
+            user,
+        };
+    }
+
+}
